a68e2ad9c80c1be5dfc98f796723e50de8a516cc
innas2019 <inna.i.shuster@gmail.com>
2022-01-04 11:51:06 +0200

    courses menu

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,40 +1,66 @@
 import React, { Component } from 'react';
 import { Card } from 'primereact/card';
 import { Panel } from 'primereact/panel';
+import {ContextMenu} from 'primereact/contextmenu';
 import Contacts from './Contacts.js';
 export default class CoursesPage extends Component {
     state = {};
-
+    course1='Тестирование ПО';
+    course2='Автоматическое тестирование' ; 
+    course3='Иврит для IT';
+    course4='Технический английский';
+    course5='Программирование на Scratch';
+    course6='Программирование на Python';
+    course7='Занимательная математика';
+    course8='Английский язык';
+    course9='JavaSctipt и основы Web-разработки';
+    
+    menu =  [
+             {label: this.course1,  url: '/courses#c1'},
+             {label: this.course2,  url: '/courses#c2'},
+             {label: this.course3,  url: '/courses#c3'},
+             {label: this.course4,  url: '/courses#c8'},
+        ];  
+    menuAdd =  [
+            {label: this.course5,  url: '/courses#c5'},
+            {label: this.course6,  url: '/courses#c4'},
+            {label: this.course7,  url: '/courses#c6'},
+            {label: this.course8,  url: '/courses#c7'},
+            {label: this.course9,  url: '/courses#c9'},
+       ];    
+    openImg(e){
+        //const posx=e.nativeEvent.offsetX;
+    }
+    
     render() {
         return ( <Panel>
-            <div  className="p-text-center p-orange p-pt-3 " >
-                <h3>Список курсов </h3>
-            </div>
-            <div  className="p-d-flex p-flex-column p-flex-md-row">
-                <div className="p-px-3">
+            
+            <div className="p-grid">
+                <div className="p-col-12 p-lg-4 p-md-6 p-px-3">
                   <p> <img src="assets/images/mainCat.jpg"  className='p-page-img' alt="картинка с котом-учителем"/></p>
-                  <h5 className="p-orange">Для взрослых: </h5>
-                   <p > <a className="p-mylink p-text-bold" href="#c1">Тестирование программного обеспечения</a></p>
-                   <p > <a className="p-mylink p-text-bold" href="#c2">QA для продвинутых</a></p>
-                   <p > <a className="p-mylink p-text-bold" href="#c3">Иврит для IT</a></p>
-                   <p > <a className="p-mylink p-text-bold" href="#c8">Технический английский</a></p>
-                   <h5 className="p-orange">Для детей: </h5>
-                   <p > <a className="p-mylink p-text-bold" href="#c4">Программирование на Python</a></p>
-                   <p > <a className="p-mylink p-text-bold" href="#c5">Программирование на Scratch</a></p>
-                   <p > <a className="p-mylink p-text-bold" href="#c6">Занимательная математика</a></p>
-                   <p > <a className="p-mylink p-text-bold" href="#c7">Английский язык</a></p>
+                  <h5> <a className="p-orange p-mylink" href="#c00">Курсы для взрослых:</a> </h5>
+                   <p > <a className="p-mylink p-text-bold" href="#c1">{this.course1}</a></p>
+                   <p > <a className="p-mylink p-text-bold" href="#c2">{this.course2}</a></p>
+                   <p > <a className="p-mylink p-text-bold" href="#c3">{this.course3}</a></p>
+                   <p > <a className="p-mylink p-text-bold" href="#c8">{this.course4}</a></p>
+                   <h5> <a className="p-orange p-mylink" href="#c01">Курсы для детей:</a> </h5>
+                  
                    <p className="p-orange">Также есть  индивидуальные занятия для взрослых и детей разной степени подготовленности </p>
                    <Contacts/>
                 </div>
-                <div className="p-pl-3 p-pr-6">
-                    <div className=" p-pr-6" ><h5 className="p-orange " ><p>Список курсов для взрослых построен таким образом, чтобы можно было выбрать курс согласно своему уровню знаний, 
-                    а также брать дополнительные тренинги при наличии желания и возможностей.</p> <p>Для тех, кто прослушал основные курсы, на тренинги предоставляется скидка. </p><p>Список курсов и тренингов постоянно обновляется.</p>
-                    </h5>
-                    <p> <img src="assets/images/courses1.jpg"  className='p-page-img' alt="схема курсов"/></p>
-                     </div> 
+                <div className="p-col-12 p-lg-8 p-md-6 p-px-3">
+                    <h3 className="p-text-center p-orange p-pt-3 ">Список курсов </h3>
+                    <div id="c00" className=" p-pr-6" ><h5 className="p-orange " ><p>Список курсов для взрослых построен таким образом, 
+                        чтобы можно было выбрать курс согласно своему уровню знаний, 
+                                    а также брать дополнительные тренинги при наличии желания и возможностей. В составе каждого курса предусмотрена практика с реальными приложениями.</p> <p>Для тех, кто прослушал основные курсы, на тренинги предоставляется скидка. </p>
+                                    <p>Список курсов и тренингов постоянно обновляется. Щелкните по рисунку правой кнопкой мыши, чтобы перейти к описанию курсов.</p>
+                            </h5>
+                            <ContextMenu className="p-addlink" model={this.menu} ref={el => this.cm = el}></ContextMenu>                    
+                            <p> <img src="assets/images/courses1.jpg"  className='p-page-img' onContextMenu={(e) => this.cm.show(e)} alt="схема курсов"/></p>
+                    </div> 
              
-                    <Card id="c1" title="QA для начинающих" subTitle="Тестирование программного обеспечения " >
-                    <div className="p-m-0" style={{lineHeight: '1.5'}}>
+                    <Card id="c1" title="QA для начинающих" subTitle={this.course1} >
+                      <div className="p-m-0" style={{lineHeight: '1.5'}}>
                         <p>Продолжительность курса - 12 мес. Язык обучения - русский.
                         </p>
                         Программа  :
@@ -61,18 +87,21 @@ export default class CoursesPage extends Component {
                                 <p> Язык программирования JavaScript,  <b>DOM</b> (Document Object Model), основы автоматизации тестирования с использованием Selenium 
                                 </p>
                                 Основа нашего курса - практика. Уже с первых занятий мы начинаем разбирать реальные задачи с собеседований, 
-                    квалификационных тестов и т.д. участвовать в реальных проектах. 
-                    По окончанию курса те, кто будет все это делать, выйдут реальными тестировщиками с приличным опытом .
-                    После 3-4 месяцев занятий можно начинать работать фрилансером-тестировщиком. Покажем и расскажем как, введем "за руку" желающих в мир QA
+                        квалификационных тестов и т.д. участвовать в реальных проектах. 
+                        По окончанию курса те, кто будет все это делать, выйдут реальными тестировщиками с приличным опытом .
+                        После 3-4 месяцев занятий можно начинать работать фрилансером-тестировщиком. Покажем и расскажем как, введем "за руку" желающих в мир QA
                                                                           
                            <p className="p-text-bold"> <a className="p-mylink" href="/news#n1" rel="noreferrer">подробнее</a>
                            </p>
                            <p><a className="p-mylink" href="https://youtu.be/8YugjIeIeu4" target="_blank" rel="noreferrer">Запись первого занятия </a></p>
+                           
                     </div>
+                    <a className="p-addlink " href="#c0">В начало<i className="pi pi-arrow-up" ></i></a>
                    </Card>
+                  
                    <img src="assets/images/hatul_gold.png" width= "10%" alt="logo with gold cat"/>
                   
-                    <Card id="c2" title="Автоматизация тестирования web-приложений на языке Java" subTitle="Тестирование программного обеспечения ">
+                    <Card id="c2" title="Автоматизация тестирования web-приложений на языке Java" subTitle={this.course2}>
                         <div className="p-m-0"style={{lineHeight: '1.5'}} >
                         <p>Язык обучения: русский, английский. Продолжительность курса - 2 мес</p>
                             <p>Курс предназначен для обучения 
@@ -92,22 +121,24 @@ export default class CoursesPage extends Component {
                                 Наша цель - дать вам необходимые знания, чтобы вы могли начать автоматизировать процесс тестирования ваших веб-приложений через пользовательский интерфейс с нуля.
                                 Требуемый уровень подготовки - понимание принципов программирования, на уровне школьной программы. Все базовые конструкции языка Java, необходимые для построения нашей системы автотестов, будут разобраны на курсе. Но, данный курс не является общим курсом обучения программированию на Java. Здесь сделан акцент именно на ознакомлении с основными шаблонами и подходами, используемыми при построении автотестов.
                         </div>
+                        <a className="p-addlink " href="#c0">В начало<i className="pi pi-arrow-up" ></i></a>
                   </Card> 
                     <img src="assets/images/hatul_green.png" width= "10%" alt = "логотип - зеленый кот"/>
-                  <Card id="c3" title="Иврит для IT"  subTitle="Подготовка к собеседованиям" >
+                  <Card id="c3" title={this.course3}  subTitle="Подготовка к собеседованиям" >
                         <p className="p-m-0" style={{lineHeight: '1.5'}}>
                           Продолжительность тренинга 9-10 занятий (2 месяца).
                         В программе : резюме, методика найма в израильском IT, профессиональная лексика на иврите, 
                         общение на профессиональные темы, аудирование.
-                        Это тренинг позволит уверенно и без стресса проходить как первоначальные этапы собеседований, так и последующие, вплоть до технического. Требуемая подготовка - уверенный ульпан א
+                        Этот тренинг позволит уверенно и без стресса проходить как первоначальные этапы собеседований, так и последующие, вплоть до технического. Требуемая подготовка - уверенный ульпан א
                         </p>
                         <p className="p-text-bold"> <a className="p-mylink" href="/news#n3">подробнее</a>
                            </p>
                            <p><a className="p-mylink" href="https://youtu.be/zFIuOEC96ME" target="_blank" rel="noreferrer">Запись первого занятия </a> </p>
+                           <a className="p-addlink " href="#c0">В начало<i className="pi pi-arrow-up" ></i></a>
                     </Card>
                     <img src="assets/images/hatul_pink.png" width= "10%" alt="логотип - пурпурный кот"/>
                   
-                    <Card id="c8" title="Технический английский"  subTitle="Для IT - индустрии" >
+                    <Card id="c8" title={this.course4}  subTitle="Для IT - индустрии" >
                         <div className="p-m-0" style={{lineHeight: '1.5'}}>
                         <p>Продолжтельность курса 4-5 месяца.</p>
                         <p>Тренинг для тех, кто изучал английский в школе и институте, но никогда не имел возможности применить знания в реальной жизни. 
@@ -128,16 +159,30 @@ export default class CoursesPage extends Component {
                         <li>Урок 9. Грамматика. Passive (Пассив). Чтение научных текстов по информатике.</li>
                         <li>Урок 10. Придаточное предложение. “Если” предложения. Direct/Reported speech (Прямая/докладная речь). Present Perfect (Настоящее совершенное)</li></ui>
                         </div>
+                        <a className="p-addlink " href="#c0">В начало<i className="pi pi-arrow-up" ></i></a>
                       </Card>         
-               
-                <div className=" p-pr-6" ><h5 className="p-orange " ><p>Список курсов для детей построен таким образом, 
-                    чтобы дети могли начать с простого визуального языка scretch, рассчитанного на самых маленьких, и постепенно перейти к настоящему программированию. 
-                    </p> <p>Ученики могут взять разные дополнительные курсы. Постоянным ученикам в нашей школе есть скидки и подарки</p><p>Список курсов и тренингов постоянно обновляется.</p>
-                    </h5>
-                    <p> <img src="assets/images/courses2.jpg"  className='p-page-img' alt="схема курсов"/></p>
-                     </div>     
+               </div>
+              
+                <div className="p-col-12 p-lg-4 p-md-6 p-px-3">
+                   <p> <img src="assets/images/red1.jpg"  className='p-news-img' alt="рисунок кота"/></p>
+                   <h5> <a className="p-orange p-mylink" href="#c01"></a>Курсы для детей: </h5>
+                   <p > <a className="p-mylink p-text-bold" href="#c5">{this.course5}</a></p>
+                   <p > <a className="p-mylink p-text-bold" href="#c4">{this.course6}</a></p>
+                   <p > <a className="p-mylink p-text-bold" href="#c6">{this.course7}</a></p>
+                   <p > <a className="p-mylink p-text-bold" href="#c7">{this.course8}</a></p>
+                   <p > <a className="p-mylink p-text-bold" href="#c9">{this.course9}</a></p>
+                </div>
+                <div className="p-col-12 p-lg-8 p-md-6 p-px-3">       
+                    <div id="c01" className=" p-pr-6" ><h5 className="p-orange " ><p>Список курсов для детей построен таким образом, 
+                        чтобы дети могли начать с простого визуального языка scretch, рассчитанного на самых маленьких, и постепенно перейти к настоящему программированию. 
+                        </p> <p>Ученики могут взять разные дополнительные курсы. Постоянным ученикам в нашей школе есть скидки и подарки</p>
+                        <p>Список курсов и тренингов постоянно обновляется. Щелкните по рисунку правой кнопкой мыши, чтобы перейти к описанию курсов.</p>
+                        </h5>
+                        <ContextMenu className="p-addlink" model={this.menuAdd} ref={el1 => this.cma = el1}></ContextMenu>                    
+                          <p> <img src="assets/images/courses2.jpg"  className='p-page-img' onContextMenu={(e) => this.cma.show(e)} alt="схема курсов"/></p>
+                    </div>     
                   
-                    <Card id="c5" title="Программирование на Scratch" 
+                    <Card id="c5" title={this.course5} 
                     subTitle="Для детей 7-10 лет" >
                         <p className="p-m-0 " style={{lineHeight: '1.5'}} >
                             Дети начинают с самых простых проектов и в ходе обучения делают все более и более сложные игры. 
@@ -175,7 +220,7 @@ export default class CoursesPage extends Component {
                            <p><a className="p-mylink" href="https://scratch.mit.edu/projects/410988460" target="_blank" rel="noreferrer">Пример проекта </a> </p>
                         </Card> 
                         <img src="assets/images/hatul_gold.png" width= "10%" alt="логотип с котом"/>
-                        <Card id="c4" title="Программирование на Python"  subTitle="Для детей 12-15 лет" >
+                        <Card id="c4" title={this.course6}  subTitle="Для детей 12-15 лет" >
                         <p className="p-m-0" style={{lineHeight: '1.5'}}>
                       Изучение языка и основ программирования.  Далее дети делают игры на библиотеке pygame. Есть группы на русском языке и на иврите
                       <ui> Программа курса: 
@@ -194,10 +239,33 @@ export default class CoursesPage extends Component {
                         </ui>
                         <p > <a className="p-mylink" href="/news#n9">Почему мы выбрали Python</a></p>
                         </p>
+                        <a className="p-addlink " href="#c0">В начало<i className="pi pi-arrow-up" ></i></a>
+                    </Card>
+                    <img src="assets/images/hatul_purple.png" width= "10%" alt="логотип с котом"/>
+                        <Card id="c9" title={this.course9}  subTitle="Для взрослых и детей от 14 лет" >
+                        <p className="p-m-0" style={{lineHeight: '1.5'}}>
+                      Изучение языка JavaSctipt и основ программирования.  Обучение на русском языке 
+                      <ui> Программа курса: 
+                           <li>переменные </li>
+                           <li>арифметические и логические операции</li>
+                           <li>условные операторы и циклы</li>
+                           <li>структуры данных</li>
+                           <li>функции</li>
+                           <li>классы</li>
+                           <li>основы объектно-ориентироованного программирования</li>
+                           <li>понятие о библиотеках и модулях</li>
+                           <li>понятие о событиях </li>
+                           <li>Основы Web: HTML, CSS</li>
+                           <li>Document Object Module и основы Web разработки</li>
+                           <li>знакомство с React и создание простых сайтов</li>
+                        </ui>
+                       
+                        </p>
+                        <a className="p-addlink " href="#c0">В начало<i className="pi pi-arrow-up" ></i></a>
                     </Card>
                         <img src="assets/images/hatul_green.png" width= "10%" alt="логотип с котом"/>
                   
-                    <Card id="c6" title="Занимательная математика" 
+                    <Card id="c6" title={this.course7} 
                     subTitle="Для детей 10-12 лет" >
                         <div className="p-m-0" style={{lineHeight: '1.5'}}>
                             <p>Авторский курс, на котором дети решают интересные задачи и узнают интересные факты. 
@@ -210,15 +278,16 @@ export default class CoursesPage extends Component {
                         </Card> 
                         <img src="assets/images/hatul_pink.png" width= "10%" alt="логотип "/>
                   
-                   <Card id="c7" title="Английский язык" 
+                   <Card id="c7" title={this.course8} 
                      subTitle="Для детей 10-12 лет" >
                         Загадки, детективные истории, спасенные принцессы. Авторский курс. Учение без мучения и зубрежки.
                         <p className="p-text-bold"> <a className="p-mylink" href="/news#n8">Пример задания</a>
                            </p>
+                           <a className="p-addlink " href="#c0">В начало<i className="pi pi-arrow-up" ></i></a>
                    </Card> 
-                   </div>
+                </div>
+            </div> 
              
-            </div>      
             </Panel>)
     }
 }
